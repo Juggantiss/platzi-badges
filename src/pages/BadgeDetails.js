b610b6786f9a5cf97bb7ec6817d96f92d4a7a4c5
Juan Raul Martinez Lopez <juggantiss@gmail.com>
2021-05-01 18:18:53 -0500

    creado custom hook para un contador e incrementarlo con useState

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import Badge from "../components/Badge";
@@ -7,7 +7,18 @@ import DeleteBadgeModal from "../components/DeleteBadgeModal";
 import confLogo from "../images/platziconf-logo.svg";
 import "./styles/BadgeDetails.css";
 
+function useIncreaseCount(max) {
+  const [count, setCount] = useState(0);
+
+  if (count > max) {
+    setCount(0);
+  }
+
+  return [count, setCount];
+}
+
 export default function BadgeDetails(props) {
+  const [count, setCount] = useIncreaseCount(4);
   const { badge } = props;
   return (
     <div>
@@ -41,6 +52,15 @@ export default function BadgeDetails(props) {
             <h2>Actions</h2>
             <div>
               <div>
+                <button
+                  onClick={() => {
+                    setCount(count + 1);
+                  }}
+                  className="btn btn-primary mr-4"
+                >
+                  Increase Count: {count}
+                </button>
+
                 <Link
                   className="btn btn-primary mb-4 mt-2"
                   to={`/badges/${badge.id}/edit`}
